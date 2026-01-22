e77a283c911c341a24c43434864dd447a58a652e
Umut Kocaman <umutkcmn@gmail.com>
2026-01-22 15:47:56 +0300

    hosgeldin

---
+++ Assets/Script/Temeller.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Temeller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Merhaba Unity'e Hoþgeldiniz.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
